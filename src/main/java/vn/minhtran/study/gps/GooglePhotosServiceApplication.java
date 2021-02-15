b564246c479cc106b3379d3477325aed3808632a
Minh Tran <wisekingtran@gmail.com>
2021-02-15 15:12:36 +0700

    Initial commit

---
+++ src/main/java/vn/minhtran/study/gps/GooglePhotosServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.minhtran.study.gps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePhotosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePhotosServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/minhtran/study/gps/GooglePhotosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.minhtran.study.gps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglePhotosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
